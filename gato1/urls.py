4313740e8166f9610e3016785de9eb2b6200cab1
mauser776 <mauser776@gmail.com>
2023-09-24 20:03:20 -0300

    commit 2

--- gato1/urls.py
+++ gato1/urls.py
@@ -17,6 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello_world)
+  
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
