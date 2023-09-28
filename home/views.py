f28f5c96562361fc193f5924a16856454de27a06
mauser776 <mauser776@gmail.com>
2023-09-28 00:41:46 -0300

    commit

--- home/views.py
+++ home/views.py
@@ -3,21 +3,33 @@ from .models import Cuadro
 
 # Create your views here.
 
+
 def home_view(request):
-    return render(request, 'home/home.html')
+
+    cuadros = Cuadro.objects.all()
+
+    context = {
+        'cuadros': cuadros,
+    }
+
+    return render(request, 'home/home.html', context)
+
 
 def proyectos_view(request):
     return render(request, 'home/proyectos.html')
 
+
 def contacto_view(request):
     return render(request, 'home/contacto.html')
+
 
 def sobre_mi_view(request):
     return render(request, 'home/sobre_mi.html')
 
+
 def galeria_view(request):
     cuadros = Cuadro.objects.all()
-    
+
     lista_cuadros = []
     for x in cuadros:
         lista_cuadros.append()
@@ -29,14 +41,13 @@ def galeria_view(request):
 
     return render(request, 'home/galeria.html', context)
 
+
 def cuadro_detail_view(request, id):
 
     cuadro = Cuadro.objects.get(id=id)
 
-
     if cuadro.cantidad_inicial is not None:
         cuadros_restantes = cuadro.cantidad_inicial - cuadro.cantidad_vendida
-        print(cuadros_restantes)   
         context = {
             'cuadro': cuadro,
             'cuadros_restantes': cuadros_restantes,
@@ -45,6 +56,5 @@ def cuadro_detail_view(request, id):
         context = {
             'cuadro': cuadro,
         }
-        
 
     return render(request, 'home/cuadro.html', context)
