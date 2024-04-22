300eaefeb2680bb2f9577f064b4dd07de82cca70
mauser776 <mauser776@gmail.com>
2024-04-22 19:47:31 -0400

    commit

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import home_view, obras_view, sobre_mi_view, contacto_view, galerias_view, cuadro_view
 
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls import url
 
 
 app_name = 'home'
@@ -15,6 +20,10 @@ urlpatterns = [
     path('galerias/', galerias_view, name='galerias-view'),
     path('cuadro/<id>/', cuadro_view, name='cuadro-view'),
 
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
