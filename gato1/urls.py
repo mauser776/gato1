4e72559758e967d4b5d7f01bee85f79d734a187a
mauser776 <mauser776@gmail.com>
2024-04-23 17:06:34 -0400

    commit

--- gato1/urls.py
+++ gato1/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.urls import re_path as url
 from django.views.static import serve
-from django.conf.urls import url
+
 
 admin.site.site_header = 'Andrés Honorato administración'
 admin.site.index_title = ''
@@ -34,7 +33,6 @@ urlpatterns = [
         {'document_root':       settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
-
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
