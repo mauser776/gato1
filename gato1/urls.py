c0d71efd3c3dce30ec275128b526350a210cd885
mauser776 <mauser776@gmail.com>
2024-04-23 16:48:40 -0400

    commit

--- gato1/urls.py
+++ gato1/urls.py
@@ -20,12 +20,20 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
+
 admin.site.site_header = 'Andrés Honorato administración'
 admin.site.index_title = ''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
+
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 
 ]
 
