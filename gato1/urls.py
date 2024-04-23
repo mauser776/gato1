967da99fbb7ee66df70f9d8bee7ebe3d741fc58a
mauser776 <mauser776@gmail.com>
2024-04-23 17:32:47 -0400

    commit

--- gato1/urls.py
+++ gato1/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import re_path as url
 from django.views.static import serve
-
 
 admin.site.site_header = 'Andrés Honorato administración'
 admin.site.index_title = ''
@@ -28,11 +26,9 @@ admin.site.index_title = ''
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
+    re_path(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT}),
 
-    url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,
-        {'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
